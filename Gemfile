5bea41275d41ce84c0b75b6f2385ebbe464dcdcb
RyoKazama <ryo.kazama1023@gmail.com>
2017-08-28 16:54:45 +0900

    faker

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ gem 'omniauth-facebook'
 
 gem 'carrierwave'
 gem 'mini_magick'
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  content = Faker::Content.sentence
+  topic = Faker::Content.sentence
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+  Topic.create!(content: content,
+                )
+  Comment.create!(comment: topic,
+                )
+end
