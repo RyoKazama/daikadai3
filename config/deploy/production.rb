0c52b871cf114d0408c332f85f4d8aa8eb82aa33
RyoKazama <ryo.kazama1023@gmail.com>
2017-09-04 22:44:37 +0900

    databese名の修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '13.114.197.108', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/ryokazama/.ssh/id_rsa'
+server '13.114.197.108', user:'app', roles:%w{app db web}
+set :ssh_options, keys:'/Users/ryokazama/.ssh/id_rsa'
