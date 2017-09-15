171de0c27cff72cdcb1151752722847259236934
RyoKazama <ryo.kazama1023@gmail.com>
2017-09-15 20:09:02 +0900

    大課題３要件直し

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
 
+  before_action :set_comment, only:[:edit,:update,:destroy,:show]
+
+
   def create
       @comment = current_user.comments.build(comments_params)
       @topic = @comment.topic
@@ -13,6 +16,19 @@ class CommentsController < ApplicationController
         end
       end
   end
+
+  def edit
+  end
+
+  def update
+      @comment = Comment.find(params[:id])
+      if @comment.update(comments_params)
+        redirect_to topic_path(@comment.topic), notice:"コメントが編集されました!"
+      else
+        redirect_to edit_topic_comment_path, notice:"コメントを編集してください!"
+      end
+  end
+
 
   def destroy
     @comment=Comment.find(params[:id])
@@ -28,5 +44,10 @@ class CommentsController < ApplicationController
     def comments_params
       params.require(:comment).permit(:topic_id, :content)
     end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
 
 end
